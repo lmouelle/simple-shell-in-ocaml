24cc36f48cadbdaeab20dd1fdccdf04a3e98a6a5
fake@email.com <fake@email.com>
2022-03-15 23:00:38 -0700

    Redo parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -1,8 +1,8 @@
 open Angstrom
 
 (*
- line: [command | pipeline] * \n
- pipeline: command ['|' command]+
+ shell language: pipeline? \n
+ pipeline: command ['|' command]*
  command: word[[\s]+ word]*
  word: token_char+
  token_char: all characters where is_token_char char = true 
@@ -20,25 +20,22 @@ let is_token_char = function
    | c when is_whitespace c -> false
    | _ -> true
 
-let whitespace_parser = take_while is_whitespace
+let word_parser = take_while1 is_token_char <?> "failed to parse word"
 
-let pipe_parser = char '|' <* whitespace_parser
+let whitespace_dropping_parser = skip_while is_whitespace
 
-let token_parser = take_while1 is_token_char <* whitespace_parser
+let command_parser = sep_by1 whitespace_dropping_parser word_parser 
+<?> "Failed to parse command" 
+>>= function
+| [] -> fail "Empty command"
+| executable :: args -> return {executable; args}
 
-let command_parser = many token_parser >>= fun tokens ->
-   match tokens with
-   | [] -> return None
-   | executable :: args -> return (Some {executable; args})
+let pipeline_parser = sep_by1 (whitespace_dropping_parser *> char '|' <* whitespace_dropping_parser) command_parser
 
-let pipeline_parser = sep_by pipe_parser command_parser >>= function
-   | [] -> return None
-   | possible_commands -> 
-      if (List.filter Option.is_none possible_commands) <> []
-      then fail "Invalid pipeline list, empty subcommand detected"
-      else 
-         let commands = List.map Option.get possible_commands in
-         return (Some commands)
+let no_op_parser = whitespace_dropping_parser >>| fun _ -> []
 
-let parse_line ln =
-   Angstrom.parse_string ~consume:All pipeline_parser (String.trim ln)
+let shell_parser = pipeline_parser <|> no_op_parser
+
+(* We really should modify the parser to accept trailing and preceding whitespace
+   instead of just trimming the string. But this is easier for now. *)
+let parse ln = parse_string ~consume:All shell_parser (String.trim ln)
