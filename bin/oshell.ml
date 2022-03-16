7112d33624fe0df6ac778b2ff4ff4e0fb64be2e8
fake@email.com <fake@email.com>
2022-03-16 00:07:20 -0700

    Apply ocamlformat

--- bin/oshell.ml
+++ bin/oshell.ml
@@ -1,26 +1,40 @@
 open Lib.Parser
 
-let oshell_cd args = 
-  match args with 
-  | [] -> Printf.eprintf "Expected an argument for change directory"; 1
-  | arg :: [] -> Unix.chdir arg; 0
-  | _ -> Printf.eprintf "Received too many args for change directory"; 1
-;;
+let oshell_cd args =
+  match args with
+  | [] ->
+      Printf.eprintf "Expected an argument for change directory";
+      1
+  | [ arg ] ->
+      Unix.chdir arg;
+      0
+  | _ ->
+      Printf.eprintf "Received too many args for change directory";
+      1
 
-let oshell_help () = 
+let oshell_help () =
   Printf.printf "Oshell: a simple ocaml shell.\nBuilt ins: cd, quit/exit\n"
-;;
 
-let builtin_functions = 
-  [("quit", fun _ -> ignore @@ exit 0; 0);
-   ("exit", fun _ -> ignore @@ exit 0; 0);
-   ("help", fun _ -> oshell_help (); 0);
-   ("cd", fun args -> oshell_cd args)]
+let builtin_functions =
+  [
+    ( "quit",
+      fun _ ->
+        ignore @@ exit 0;
+        0 );
+    ( "exit",
+      fun _ ->
+        ignore @@ exit 0;
+        0 );
+    ( "help",
+      fun _ ->
+        oshell_help ();
+        0 );
+    ("cd", fun args -> oshell_cd args);
+  ]
 ;;
-
 
 let waitforprocess pid =
-  let (_, status) = Unix.waitpid [Unix.WUNTRACED] pid in
+  let _, status = Unix.waitpid [ Unix.WUNTRACED ] pid in
   match status with
   | Unix.WEXITED exitcode -> exitcode
   | Unix.WSIGNALED exitcode -> exitcode
@@ -28,82 +42,70 @@ let waitforprocess pid =
 in
 
 let rec exec_pipeline = function
-| [] -> 0
-| ({executable; args} :: []) -> 
-  begin
-    match List.assoc_opt executable builtin_functions with
-    | Some func -> func args
-    | None ->
-      begin
-        let argv = Array.of_list (executable :: args) in
-        let pid = (Unix.fork ()) in
-        if pid = 0 then Unix.execvp executable argv
-        else if pid < 0 then (Printf.eprintf "Failure forking for process %s" executable; 1)
-        else waitforprocess pid
-      end
-  end
-| ({executable; args} :: remainder) ->
-    begin
+  | [] -> 0
+  | [ { executable; args } ] -> (
+      match List.assoc_opt executable builtin_functions with
+      | Some func -> func args
+      | None ->
+          let argv = Array.of_list (executable :: args) in
+          let pid = Unix.fork () in
+          if pid = 0 then Unix.execvp executable argv
+          else if pid < 0 then (
+            Printf.eprintf "Failure forking for process %s" executable;
+            1)
+          else waitforprocess pid)
+  | { executable; args } :: remainder ->
       let fd_in, fd_out = Unix.pipe () in
       let argv = Array.of_list (executable :: args) in
       let pid = Unix.fork () in
-      if pid = 0 then
-        begin
-          Unix.dup2 fd_out Unix.stdout;
-          Unix.close fd_out;
-          Unix.close fd_in;
-          Unix.execvp executable argv
-        end
-      else if pid < 0 then (Printf.eprintf "Failure forking for process %s" executable; 1)
-      else 
-        begin
-          Unix.dup2 fd_in Unix.stdin;
-          Unix.close fd_in;
-          Unix.close fd_out;
-          exec_pipeline remainder
-        end
-    end
+      if pid = 0 then (
+        Unix.dup2 fd_out Unix.stdout;
+        Unix.close fd_out;
+        Unix.close fd_in;
+        Unix.execvp executable argv)
+      else if pid < 0 then (
+        Printf.eprintf "Failure forking for process %s" executable;
+        1)
+      else (
+        Unix.dup2 fd_in Unix.stdin;
+        Unix.close fd_in;
+        Unix.close fd_out;
+        exec_pipeline remainder)
 in
 
 let run (line : string) =
   match parse line with
-  | Error msg -> Printf.eprintf "Error parsing commands, %s" msg; 1
+  | Error msg ->
+      Printf.eprintf "Error parsing commands, %s" msg;
+      1
   | Ok commands -> exec_pipeline commands
 in
 
-let run_file filename = 
+let run_file filename =
   let chan = open_in filename in
   try
-    while true; do
+    while true do
       let ln = input_line chan in
-      ignore (run ln);
-    done; 
-  with End_of_file ->
-    close_in chan;
+      ignore (run ln)
+    done
+  with End_of_file -> close_in chan
 in
 
 let run_prompt () =
   try
-    while true; do
+    while true do
       Printf.printf "> ";
       let ln = read_line () in
-      ignore (run ln);
-    done; 
+      ignore (run ln)
+    done
   with End_of_file -> ()
 in
 
 (* Top level *)
-if Array.length Sys.argv > 2 then
-  begin
-   Printf.printf "Usage: oshell [script]";
-   exit 64
-  end
+if Array.length Sys.argv > 2 then (
+  Printf.printf "Usage: oshell [script]";
+  exit 64)
 else if Array.length Sys.argv == 2 then
-  begin
-    let filename = Array.get Sys.argv 0 in
-    run_file filename
-  end
-else
-  begin
-    run_prompt ()
-  end
+  let filename = Array.get Sys.argv 0 in
+  run_file filename
+else run_prompt ()
--- lib/parser.ml
+++ lib/parser.ml
@@ -8,32 +8,30 @@ open Angstrom
  token_char: all characters where is_token_char char = true 
 *)
 
-type command = {
-   executable: string;
-   args: string list;
-}
+type command = { executable : string; args : string list }
 
-let is_whitespace = (function ' ' | '\t' | '\n' -> true | _ -> false)
+let is_whitespace = function ' ' | '\t' | '\n' -> true | _ -> false
 
 let is_token_char = function
-   | '|' -> false
-   | c when is_whitespace c -> false
-   | _ -> true
+  | '|' -> false
+  | c when is_whitespace c -> false
+  | _ -> true
 
 let word_parser = take_while1 is_token_char <?> "failed to parse word"
-
 let whitespace_dropping_parser = skip_while is_whitespace
 
-let command_parser = sep_by1 whitespace_dropping_parser word_parser 
-<?> "Failed to parse command" 
->>= function
-| [] -> fail "Empty command"
-| executable :: args -> return {executable; args}
+let command_parser =
+  sep_by1 whitespace_dropping_parser word_parser <?> "Failed to parse command"
+  >>= function
+  | [] -> fail "Empty command"
+  | executable :: args -> return { executable; args }
 
-let pipeline_parser = sep_by1 (whitespace_dropping_parser *> char '|' <* whitespace_dropping_parser) command_parser
+let pipeline_parser =
+  sep_by1
+    (whitespace_dropping_parser *> char '|' <* whitespace_dropping_parser)
+    command_parser
 
 let no_op_parser = whitespace_dropping_parser >>| fun _ -> []
-
 let shell_parser = pipeline_parser <|> no_op_parser
 
 (* We really should modify the parser to accept trailing and preceding whitespace
